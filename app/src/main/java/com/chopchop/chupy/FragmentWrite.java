c04f0d1a99e8fe3e325cf228a7565842c1980675
Daniyal Ahmad Rizaldhi <onieln14@gmail.com>
2018-07-25 10:40:56 +0700

    update - add emergence page

--- app/src/main/java/com/chopchop/chupy/FragmentWrite.java
+++ app/src/main/java/com/chopchop/chupy/FragmentWrite.java
@@ -23,7 +23,7 @@ public class FragmentWrite extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_write, container, false);
+        return inflater.inflate(R.layout.emergency_page, container, false);
     }
 
 }
